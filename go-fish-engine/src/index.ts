42e960318365412766590ddb2ccf1bbeec0a03ad
Cedar Edwards <daedwards@vmware.com>
2021-03-04 11:40:03 -0500

    Restructure game engine code
    This lets functions invoke each other without using 'this'

--- go-fish-engine/src/index.ts
+++ go-fish-engine/src/index.ts
@@ -36,10 +36,66 @@ export function GoFishGame(
     let _nextPlayerId = 0
     let _deck: Array<Card> = deck || []
     const _players: { [key: string]: PlayerState } = players || {}
-
     const sortedPlayerIds = () => Object.keys(_players).sort()
-
     let _currentTurn: string | undefined = currentTurn || sortedPlayerIds()[0]
+
+    function currentState() {
+        return {
+            deck: _deck,
+            players: _players,
+            currentTurn: _currentTurn
+        };
+    }
+
+    function setDeck(deck) {
+        _deck = deck
+    }
+
+    function addPlayer(): string {
+        while(_players[`player-${_nextPlayerId}`]) {
+            _nextPlayerId++
+        }
+        const playerId = `player-${_nextPlayerId}`
+
+        _players[playerId] = {
+            hand: [],
+            sets: [],
+        }
+
+        if(!_currentTurn) _currentTurn = playerId
+
+        return playerId
+    }
+
+    function renamePlayer(playerId: string, name: string): void {
+        _players[playerId].name = name
+    }
+
+    function draw(playerName: string): void {
+        if (_deck.length === 0) return;
+        _players[playerName].hand.push(_deck[0])
+        _deck = _deck.slice(1)
+    }
+
+    function give(fromPlayer: string, toPlayer: string, cardId: number): void {
+        const card = _players[fromPlayer].hand.find(card => card.id === cardId)
+        _players[fromPlayer].hand = _players[fromPlayer].hand.filter(card => card.id !== cardId)
+        _players[toPlayer].hand.push(card)
+    }
+
+    function score(playerName: string, cardIds: number[]): void {
+        if(cardIds.length !== 3) return
+        const cards = cardIds.map(cardId =>
+            _players[playerName].hand.find(card => card.id === cardId)
+        )
+        if(cards.some(card => !card)) return
+        if(!cards.every(card => card.value === cards[0].value)) return
+
+        _players[playerName].sets.push(cards)
+        _players[playerName].hand = _players[playerName].hand.filter(card =>
+            !cardIds.includes(card.id)
+        )
+    }
 
     function endTurn(): void {
         const playerList = sortedPlayerIds()
@@ -50,79 +106,29 @@ export function GoFishGame(
         _currentTurn = playerList[nextPlayerIndex]
     }
 
+    function removePlayer(playerId: string): void {
+        const playerInfo = _players[playerId]
+        if(!playerInfo) return
+
+        if(_currentTurn === playerId) endTurn()
+
+        const recoveredCards = playerInfo.sets
+            .reduce((cards, set) => cards.concat(set), [])
+            .concat(playerInfo.hand)
+        _deck = _deck.concat(recoveredCards)
+
+        delete _players[playerId]
+    }
+
     return {
-        currentState() {
-            return {
-                deck: _deck,
-                players: _players,
-                currentTurn: _currentTurn
-            };
-        },
-
-        setDeck(deck) {
-            _deck = deck
-        },
-
-        addPlayer(): string {
-            while(_players[`player-${_nextPlayerId}`]) {
-                _nextPlayerId++
-            }
-            const playerId = `player-${_nextPlayerId}`
-
-            _players[playerId] = {
-                hand: [],
-                sets: [],
-            }
-
-            if(!_currentTurn) _currentTurn = playerId
-
-            return playerId
-        },
-
-        renamePlayer(playerId: string, name: string): void {
-            _players[playerId].name = name
-        },
-
-        draw(playerName: string): void {
-            if (_deck.length === 0) return;
-            _players[playerName].hand.push(_deck[0])
-            _deck = _deck.slice(1)
-        },
-
-        give(fromPlayer: string, toPlayer: string, cardId: number): void {
-            const card = _players[fromPlayer].hand.find(card => card.id === cardId)
-            _players[fromPlayer].hand = _players[fromPlayer].hand.filter(card => card.id !== cardId)
-            _players[toPlayer].hand.push(card)
-        },
-
-        score(playerName: string, cardIds: number[]): void {
-            if(cardIds.length !== 3) return
-            const cards = cardIds.map(cardId =>
-                _players[playerName].hand.find(card => card.id === cardId)
-            )
-            if(cards.some(card => !card)) return
-            if(!cards.every(card => card.value === cards[0].value)) return
-
-            _players[playerName].sets.push(cards)
-            _players[playerName].hand = _players[playerName].hand.filter(card =>
-                !cardIds.includes(card.id)
-            )
-        },
-
+        currentState,
+        setDeck,
+        addPlayer,
+        renamePlayer,
+        draw,
+        give,
+        score,
         endTurn,
-
-        removePlayer(playerId: string): void {
-            const playerInfo = _players[playerId]
-            if(!playerInfo) return
-            
-            if(_currentTurn === playerId) endTurn()
-
-            const recoveredCards = playerInfo.sets
-                .reduce((cards, set) => cards.concat(set), [])
-                .concat(playerInfo.hand)
-            _deck = _deck.concat(recoveredCards)
-
-            delete _players[playerId]
-        },
+        removePlayer,
     }
 }
